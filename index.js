98bf9b418636a7f2988714bf8a8517d3bfdd63a1
pragatisingh1116 <pragatithakur7898@gmail.com>
2022-10-23 18:58:09 +0530

    feat : fixed bugs

--- index.js
+++ index.js
@@ -1,51 +1,47 @@
-const express = require('express');
+const express = require("express");
 const mongoose = require("mongoose");
-const Contact = require("./models/contact")
+const Contact = require("./models/contact");
 
 const app = express();
 port = 8000;
 
-// taking data form the form 
-app.use(express.urlencoded())
+// taking data form the form
+app.use(express.urlencoded());
 
-// setting up views here 
+// setting up views here
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
 // setting up permanent routes
 app.use("/", require("./routes/index"));
 
-
-// setting up port listening 
+// setting up port listening
 app.listen(port, function (err) {
-    if (err) {
-        console.log("Error in running port", err);
-    }
-    console.log("server is up on port http://localhost:8000 ");
+  if (err) {
+    console.log("Error in running port", err);
+  }
+  console.log("server is up on port http://localhost:8000 ");
 });
 
 // files last line is here const express = require("express");
-const cookieParser = require('cookie-parser'); 
-const chalk = require('chalk');
+const cookieParser = require("cookie-parser");
+const chalk = require("chalk");
 const app = express();
 const port = 8000;
 
 //setting up the cookie
 app.use(cookieParser());
 
-
 // reading the body form data through body-parser
 // app.use(express.urlencoded());
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }))
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // calling my css files and images
 app.use(express.static("./assets"));
 
-
-// express layout using here 
-const expressLayouts = require('express-ejs-layouts');
+// express layout using here
+const expressLayouts = require("express-ejs-layouts");
 app.use(expressLayouts);
 
 app.set("layout extractStyles", true);
@@ -54,24 +50,28 @@ app.set("layout extractScripts", true);
 // calling Database
 const db = require("./config/mongoose");
 
-//setting up the ejs views 
-app.set('view engine', "ejs");
-app.set('views', './views');
-
+//setting up the ejs views
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 //setting up the router
 app.use("/", require("./routes/index"));
 
-
-
 // listen the port here
 
 app.listen(port, function (err) {
-    if (err) {
-        console.log(`Error in running server ${err} `);
-        return;
-    }
-    console.log(chalk.bgWhite.red.underline(` Server is up-to-date on port ${port} | :) =>-----------------------------------------------------------------------`));
-    console.log(chalk.bgBlueBright.yellow.underline(` Sever is on http://localhost:8000 | :) =>-----------------------------------------------------------------------`));
-
-})
+  if (err) {
+    console.log(`Error in running server ${err} `);
+    return;
+  }
+  console.log(
+    chalk.bgWhite.red.underline(
+      ` Server is running date on port ${port} | :) =>-----------------------------------------------------------------------`
+    )
+  );
+  console.log(
+    chalk.bgBlueBright.yellow.underline(
+      ` Sever is on http://localhost:8000 | :) =>-----------------------------------------------------------------------`
+    )
+  );
+});
--- routes/admin.js
+++ routes/admin.js
@@ -5,6 +5,8 @@ const homeController = require("../controllers/homeController");
 
 router.get("/", homeController.home);
 router.post("/create-session", homeController.createSession);
+router.post("/get-session", homeController.createSession);
+router.post("/make-session-env", homeController.createSession);
 
 
 module.exports = router;
